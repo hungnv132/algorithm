a0db17ce9f4c7a686d50ea32976b697afca5b3cf
hungnv132 <hungnv132@gmail.com>
2017-01-20 16:37:26 +0700

    create 'binary_search.py' file

---
+++ recursion/binary_search.py
@@ -0,0 +1,14 @@
+"""
+ + Describe:
+    - Binary search algorith is used to efficently locate a target value within a sorted sequence of n elements.
+    - When the sequence is unsorted, the stardard method is use a loop => sequential search algorithm (O(n))
+    - When the sequence is sorted and indexable => should use binary search algorithm (O(log n)
+ + Ideas:
+    - The algorithm remains 2 parameters,'low' and 'high', such that all the candidate entries have index at least low
+      and at most high. Initially, low = and high = n - 1
+    - mid = (low + high)/2
+ + Input:
+"""
+
+
+if __name__ == '__main__': pass
