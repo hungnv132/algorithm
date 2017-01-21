d57768b455ba9edaf1b48cc14059287fa7270b03
hungnv132 <hungnv132@gmail.com>
2017-01-21 23:58:22 +0700

    update binary_search.py file

--- recursion/binary_search.py
+++ recursion/binary_search.py
@@ -7,8 +7,53 @@
     - The algorithm remains 2 parameters,'low' and 'high', such that all the candidate entries have index at least low
       and at most high. Initially, low = and high = n - 1
     - mid = (low + high)/2
- + Input:
+    - data[mid] divides the list into 2 parts
+        - target_value = data[mid] => OK,
+        - target_value < data[mid] => continue recurring in 'below part'
+        - target_value > data[mid] => to recur in the other part
+ + Base Cases(recursion):stop recurring when can not divide the list or found the target value
 """
 
+# the list is sorted
 
-if __name__ == '__main__': pass
+
+def binary_search_v1(list, finding_value):
+    low = 0                 # first element's position in the list
+    high = len(list) - 1    # ending element's position in the list
+
+    if low > high:
+        return False        # recuring until list only have one element, low = 0; high = -1
+    else:
+        mid = (low + high) // 2  # middle position of the list
+        if finding_value == list[mid]:
+            return True
+        elif finding_value < list[mid]:
+            list = list[low:mid]
+            return binary_search_v1(list, finding_value)
+        else:
+            list = list[mid+1:high+1]
+            return binary_search_v1(list, finding_value)
+
+
+def find_element(list, finding_value, low, high):
+    if low > high:
+        return False        # recuring until list only have one element, low = 0; high = -1
+    else:
+        mid = (low + high) // 2
+        if finding_value == list[mid]:
+            return True
+        elif finding_value < list[mid]:
+            return find_element(list, finding_value, low, mid-1)
+        else:
+            return find_element(list, finding_value, mid + 1, high)
+
+
+def binary_search_v2(list, finding_value):
+    return  find_element(list, finding_value, 0, len(list) - 1)
+
+
+
+if __name__ == '__main__':
+    list = [12, 21, 35, 40, 56, 78, 88, 90, 99]
+    print(binary_search_v1(list, 21))
+    print(list)
