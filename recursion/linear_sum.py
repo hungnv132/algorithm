09a85759fada7aa12f81986e62c0fb88e53757f8
hungnv132 <hungnv132@gmail.com>
2017-01-23 11:35:09 +0700

    change 'linear_sum' to '_linear_sum'

--- recursion/linear_sum.py
+++ recursion/linear_sum.py
@@ -1,14 +1,14 @@
 
 
 # Performance is O(n)
-def linear_sum(S, n):
+def _linear_sum(S, n):
     if n==0:
         return 0
     else:
-        return S[n-1] + linear_sum(S, n-1)
+        return S[n-1] + _linear_sum(S, n-1)
 
 def sum(S):
-    return linear_sum(S, len(S))
+    return _linear_sum(S, len(S))
 
 if __name__ == '__main__':
     S = [1, 2, 3, 4, 5]
