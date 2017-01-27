f7cb0b1931a1b9577fe0fd271b7467bacc0c6089
hungnv132 <hungnv132@gmail.com>
2017-01-27 08:38:52 +0700

    creat 'Reverse' class

--- basic_python/main_go.py
+++ basic_python/main_go.py
@@ -3,3 +3,23 @@ from basic_python.models import Person
 list = [1 ,2, 3, 4, 5, 6, 7, 8]
 
 print(list[4:2])
+
+class Reverse(object):
+    def __init__(self, data):
+        self.data = data
+        self.index = len(data)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index == 0:
+            raise StopIteration
+        self.index -= 1
+        return self.data[self.index]
+
+if __name__ == '__main__':
+    rev = Reverse('spam')
+    # iter(rev)
+    for char in rev:
+        print(char)
