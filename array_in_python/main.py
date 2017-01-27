9d9a999529372e3b2b5e707e311dfbad6fca9380
hungnv132 <hungnv132@gmail.com>
2017-01-28 01:31:01 +0700

    add 'getsizeof' method

--- array_in_python/main.py
+++ array_in_python/main.py
@@ -1,6 +1,9 @@
 from array import array
-
+import sys
 int_nums = array('i', [1, 2, 3, 4])
+list = [1, 2, 3, 4]
+print(sys.getsizeof(int_nums))
+print(sys.getsizeof(list))
 print(int_nums)  # array('i', [1, 2, 3, 4])
 int_nums.append(5)
 print(int_nums)  # array('i', [1, 2, 3, 4, 5])
