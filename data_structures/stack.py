c671b72df65605d677506439c09036e6196d66f5
hungnv132 <hungnv132@gmail.com>
2017-01-28 22:34:32 +0700

    create 'stack.py' file

---
+++ data_structures/stack.py
@@ -0,0 +1,47 @@
+
+"""
+ + Last-in, First-out (LIFO)
+"""
+
+
+class Stack(object):
+
+    def __init__(self):
+        self._data = []
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, value):
+        self._data.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        return self._data.pop()
+
+    def top(self):
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        return self._data[-1]
+
+    def __len__(self):
+        return len(self._data)
+
+
+if __name__ == '__main__':
+    stack = Stack()
+    stack.push(4)
+    stack.push(2)
+    stack.push(3)
+    stack.push(1)
+    print(len(stack))
+    print(stack.top())
+    print(stack.is_empty())
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+    print(len(stack))
+    print(stack.is_empty())
+
