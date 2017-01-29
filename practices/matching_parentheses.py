62ce7d27813fef5ee548b9daf2289621edecf44b
hungnv132 <hungnv132@gmail.com>
2017-01-29 10:52:26 +0700

    add "is_match_v2(...)" method

--- practices/matching_parentheses.py
+++ practices/matching_parentheses.py
@@ -7,7 +7,7 @@ from data_structures.stack import Stack
 def is_match_v1(string):
 
     opening_list = []  # store opening
-    ending_list = []  # store ending
+    closing_list = []  # store closing
 
     if len(string) == 0:
         return False
@@ -16,19 +16,37 @@ def is_match_v1(string):
         if char in ['(', '{', '[']:
             opening_list.append(char)
         elif char in [')', '}', ']']:
-            ending_list.append(char)
+            closing_list.append(char)
 
     print(opening_list)
-    print(ending_list)
+    print(closing_list)
 
-    if len(opening_list) != len(ending_list):
+    if len(opening_list) != len(closing_list):
         return False
 
     for index in range(len(opening_list)):
-        if opening_list[index] != ending_list[index]:
+        if opening_list[index] != closing_list[index]:
             return False
     return True
 """
 
+
+# THIS WAY IS RIGHT!
+def is_match_v2(string):
+    
+    opening = '({['
+    closing  = ')}]'
+    if len(string) == 0:
+        return False
+
+    opening_stack = Stack()
+    for char in string:
+        if char in opening:
+            opening_stack.push(char)
+        elif char in closing:
+            if opening_stack.is_empty() or (not closing.index(char) == opening.index(opening_stack.pop())):
+                return False
+    return opening_stack.is_empty()
+
 if __name__ == '__main__':
-    print(is_match_v1("{}(()()){{{}}}[]()([])"))
+    print(is_match_v2("{}(()()){{{}}}[]()(][)"))
