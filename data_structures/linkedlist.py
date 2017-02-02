2941b22694bf86d5bc8c16173efcaa5a219a77fc
hungnv132 <hungnv132@gmail.com>
2017-02-02 14:47:49 +0700

    add class 'LinkedQueue'

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -20,8 +20,8 @@ class LinkedStack(object):
     def is_empty(self):
         return self._size == 0
 
-    def push(self, value):
-        new_node = _Node(value,self._head)
+    def push(self, element):
+        new_node = _Node(element, self._head)
         self._head = new_node
         self._size += 1
 
@@ -40,11 +40,66 @@ class LinkedStack(object):
 
     def __str__(self):
         node = self._head
-        value_list = []
+        element_list = []
         while node is not None:
-            value_list.append(node._element)
+            element_list.append(node._element)
             node = node._next
-        return str(value_list)
+        return str(element_list)
+
+
+class LinkedQueue(object):
+
+    def __init__(self):
+        self._head = None
+        self._tail = None
+        self._size = 0
+
+    def __len__(self):
+        return self._size
+
+    def is_empty(self):
+        return self._size == 0
+
+    def enqueue(self, element):
+        """Add an element to the back of queue Q"""
+
+        new_node = _Node(element, None)
+        if self.is_empty():
+            self._head = new_node
+        else:
+            self._tail._next = new_node
+        self._tail = new_node
+        self._size += 1
+
+    def dequeue(self):
+        """Remove and return the first element from the queue"""
+        """
+        if self.is_empty():
+            raise Exception('The queue is empty')
+        elif self._size == 1:
+            self._head = self._tail = None
+        else:
+            self._head = self._head._next
+        answer = self._head._element
+        self._size -= 1
+        return answer
+        """
+        if self.is_empty():
+            raise Exception('The queue is empty')
+        answer =  self._head._element
+        self._head = self._head._next
+        self._size -= 1
+        if self.is_empty():
+            self._tail = None
+        return answer
+
+    def first(self):
+        """Return the element at the front of the queue, but do not remove"""
+        if self.is_empty():
+            raise Exception('The queue is empty')
+        return self._head._element
+
+
 
 
 if __name__ == '__main__':
