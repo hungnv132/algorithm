b5915aacaaf1b7b9a4596f033beb7bc7bd0119a2
hungnv132 <hungnv132@gmail.com>
2017-02-03 16:26:53 +0700

    add classes: 'LinkedDeque'

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -25,13 +25,13 @@ class DoublyLinkedBase(object):
         """Return True if list is empty"""
         return self._size == 0
 
-    def insert_between(self, element, predecessor, successor):
+    def _insert_between(self, element, predecessor, successor):
         new_node = _Node(element, predecessor, successor)
         predecessor._next = new_node
         successor._prev = new_node
         self._size += 1
 
-    def delete_node(self, node):
+    def _delete_node(self, node):
         """Delete nonsentinel node from the list and return its element."""
         predcessor = node._prev
         successor = node._next
@@ -41,3 +41,35 @@ class DoublyLinkedBase(object):
         element = node._element
         node._prev = node._next = node._element = None
         return element
+
+class LinkedDeque(DoublyLinkedBase):
+
+    def first(self):
+        if self.is_empty():
+            raise Exception('The Deque is empty')
+        return self._header._next._element
+
+    def last(self):
+        if self.is_empty():
+            raise Exception('The Deque is empty')
+        return self._trailer._prev._element
+
+    def insert_first(self, element):
+        """Add an element to the front of the deque"""
+        self._insert_between(element, self._header, self._header._next)
+
+    def insert_last(self, element):
+        """Add an element to the back of the deque"""
+        self._insert_between(element, self._trailer._prev, self._trailer)
+
+    def delete_first(self):
+        """remove and return the element from the front of the deque"""
+        if self.is_empty():
+            raise Exception('The Deque is empty')
+        return self._delete_node(self._header._next)
+
+    def delete_last(self):
+        """Remove and return the element from the back of the deque"""
+        if self.is_empty():
+            raise Exception('The Deque is empty')
+        return self._delete_node(self._trailer._prev)
