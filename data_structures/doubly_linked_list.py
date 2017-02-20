fc72ee60ecce5be44ec4221c24eaa7d01a7380b8
hungnv132 <hungnv132@gmail.com>
2017-02-20 17:54:30 +0700

    create class 'PositionalList'

--- data_structures/doubly_linked_list.py
+++ data_structures/doubly_linked_list.py
@@ -8,7 +8,7 @@ class _Node(object):
         self._next = _next       # next node reference
 
 
-class DoublyLinkedBase(object):
+class _DoublyLinkedBase(object):
     """ A base class providing a doubly linked list representation."""
     def __init__(self):
         self._header = _Node(None, None, None)
@@ -43,7 +43,7 @@ class DoublyLinkedBase(object):
         return element
 
 
-class LinkedDeque(DoublyLinkedBase):
+class LinkedDeque(_DoublyLinkedBase):
 
     def first(self):
         if self.is_empty():
@@ -74,3 +74,84 @@ class LinkedDeque(DoublyLinkedBase):
         if self.is_empty():
             raise Exception('The Deque is empty')
         return self._delete_node(self._trailer._prev)
+
+
+class PositionalList(_DoublyLinkedBase):
+    """A sequential container of elements allowing positonal access"""
+
+# ------------------------------- START  Position class -------------------------------
+    class Position:
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position"""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location"""
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)
+
+# ------------------------------- END  Position class -------------------------------
+    def _validate(self, p):
+        """Return position's node, or raise appropriate error if invalid"""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise TypeError('p does not belong to this container')
+        if p._node._next is None:
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        """Return Position instance for given node (or None if sentinel)"""
+        if node is self._header or node is self._trailer:
+            return None
+        else:
+            return self.Position(self, node)
+
+    def first(self):
+        """Return the first Position in the list(or None if list is empty)"""
+        return self._make_position(self._header._next)
+
+    def last(self):
+        """Return the last Position in the list(or None if list is empty)"""
+        return self._make_position(self._trailer._prev)
+
+    def before(self, p):
+        """Return the Position just before Postion p (or None if p is first)"""
+        node = self._validate(p)
+        return self._make_position(node._prev)
+
+    def after(self, p):
+        """Return the Postion just after Position p(or None if p is last)"""
+        node = self._validate(p)
+        return self._make_position(node._next)
+
+    def __iter__(self):
+        """Generate a forward interation of the elements of the list."""
+        cursor = self.first()
+        while cursor is not None:
+            yield cursor.element()
+            cursor = self.after(cursor)
+
+    # override inherited version to return Position, rather than Node
+    def _insert_between(self, element, predecessor, successor):
+        """Add an element between existing nodes and return new Position"""
+        node =  super()._insert_between(element, predecessor, successor)
+        return self._make_position(node)
+
+    def add_first(self, e):
+        """Insert element e at the front of the list and return new Positon"""
+        return self._insert_between(e, self._header, self._header._next)
+
+    def add_last(self, e):
+        """Insert element e at the back of the list and return new Postion"""
+        return self._insert_between(e, self._trailer._prev, self._trailer)
+
