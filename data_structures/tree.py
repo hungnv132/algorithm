01595082ebe45b6b68ef6074374f39d74adc1d15
hungnv132 <hungnv132@gmail.com>
2017-03-09 22:56:12 +0700

    add "_add_right() and _replace()" methods in tree.py

--- data_structures/tree.py
+++ data_structures/tree.py
@@ -98,92 +98,108 @@ class LinkedBinaryTree(BinaryTree):
             self._left = left
             self._right = right
 
-        class Position(BinaryTree.Position):
-            """An abstraction representing the location of a single element"""
+    class Position(BinaryTree.Position):
+        """An abstraction representing the location of a single element"""
 
-            def __init__(self, container, node):
-                """Constructor should not be invoked by user"""
-                self._container = container
-                self._node = node
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user"""
+            self._container = container
+            self._node = node
 
-            def element(self):
-                """Return the element stored at this Position"""
-                return self._node._element
+        def element(self):
+            """Return the element stored at this Position"""
+            return self._node._element
 
-            def __eq__(self, other):
-                """Return True if other is a Position representing the same location"""
-                return type(other) is type(self) and other._node is self._node
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location"""
+            return type(other) is type(self) and other._node is self._node
 
-        def _validate(self, p):
-            """Return associated node, if position is valid"""
-            if not isinstance(p, self.Position):
-                raise TypeError('p must be proper Position type')
-            if p._container is not self:
-                raise ValueError('p does not belong to this container')
-            if p._node._parent is p._node:
-                raise ValueError('p is no longer valid')
-            return p._node
+    def _validate(self, p):
+        """Return associated node, if position is valid"""
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._parent is p._node:
+            raise ValueError('p is no longer valid')
+        return p._node
 
-        def _make_postion(self, node):
-            """Return Position instance for given node (or None if no node)"""
-            return self.Position(self, node) if node is not None else None
+    def _make_postion(self, node):
+        """Return Position instance for given node (or None if no node)"""
+        return self.Position(self, node) if node is not None else None
 
-        # -------------- binary tree constructor-----------
-        def __init__(self):
-            """Create an initally empty binary tree"""
-            self._root = None
-            self._size = 0
+    # -------------- binary tree constructor-----------
+    def __init__(self):
+        """Create an initally empty binary tree"""
+        self._root = None
+        self._size = 0
 
-        # -------------- public accessors--------------------
-        def __len__(self):
-            """Return the total number of elements in the tree"""
-            return self._size
+    # -------------- public accessors--------------------
+    def __len__(self):
+        """Return the total number of elements in the tree"""
+        return self._size
 
-        def root(self):
-            """Return the root Position of the tree (or None if tree is empty)"""
-            return self._make_postion(self._root)
+    def root(self):
+        """Return the root Position of the tree (or None if tree is empty)"""
+        return self._make_postion(self._root)
 
-        def parent(self, p):
-            """Return the Postion of p's parent (or None if p is root)"""
-            node = self._validate(p)
-            return self._make_postion(node._parent)
+    def parent(self, p):
+        """Return the Postion of p's parent (or None if p is root)"""
+        node = self._validate(p)
+        return self._make_postion(node._parent)
 
-        def left(self, p):
-            """Return the Position of p's left child (or None if no left child)"""
-            node = self._validate(p)
-            return self._make_postion(node._left)
+    def left(self, p):
+        """Return the Position of p's left child (or None if no left child)"""
+        node = self._validate(p)
+        return self._make_postion(node._left)
 
-        def right(self, p):
-            """Return the Position of p's right child (or None if no right child)"""
-            node = self._validate(p)
-            return self._make_postion(node._right)
+    def right(self, p):
+        """Return the Position of p's right child (or None if no right child)"""
+        node = self._validate(p)
+        return self._make_postion(node._right)
 
-        def num_children(self, p):
-            """Return the number of children of Position p"""
-            node = self._validate(p)
-            count = 0
-            if node._left  is not None:
-                count += 1
-            if node._right is not None:
-                count += 1
-            return count
+    def num_children(self, p):
+        """Return the number of children of Position p"""
+        node = self._validate(p)
+        count = 0
+        if node._left  is not None:
+            count += 1
+        if node._right is not None:
+            count += 1
+        return count
 
-        def _add_root(self, e):
-            """ Place element e at the root of an empty tree and return new Position
-                Raise ValueError if tree nonempty"""
+    def _add_root(self, e):
+        """ Place element e at the root of an empty tree and return new Position
+            Raise ValueError if tree nonempty"""
 
-            if self._root is not None: raise ValueError('Root exists')
-            self._size = 1
-            self._root = self._Node(e)
-            return self._make_postion(self._root)
+        if self._root is not None: raise ValueError('Root exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_postion(self._root)
 
-        def _add_left(self, p, e):
-            """ Create a new left child for Position p, storing element e.
-                Return the Position of new node.
-                Raise ValueError if Position p is invalid or p already has a left child"""
+    def _add_left(self, p, e):
+        """ Create a new left child for Position p, storing element e.
+            Return the Position of new node.
+            Raise ValueError if Position p is invalid or p already has a left child"""
 
-            node = self._validate(p)
-            if node._left is not None: raise ValueError('Left child exists')
-            self._size += 1
-            node._left =  self._Node(e, parent = node)
-            return self._make_postion(node._left)
+        node = self._validate(p)
+        if node._left is not None: raise ValueError('Left child exists')
+        self._size += 1
+        node._left =  self._Node(e, parent = node)
+        return self._make_postion(node._left)
+
+    def _add_right(self, p, e):
+        """Crete a new right child for Position p, storing element e"""
+
+        node = self._validate(p)
+        if node._right is not None: raise ValueError('Right child exists')
+        self._size += 1
+        node._right = self._Node(e, node)
+        return self._make_postion(node._right)
+
+    def _replace(self, p, e):
+        """replace the element at position p with e, and return old element."""
+        node = self._validate(p)
+        old = node._element
+        node._element = e
+        return old
