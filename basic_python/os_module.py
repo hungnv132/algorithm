82807400ae075cd0036c129a75429d4efe92de5b
hungnv132 <hungnv132@gmail.com>
2017-03-19 16:14:40 +0700

    create 'os_module.py' file

---
+++ basic_python/os_module.py
@@ -0,0 +1,8 @@
+import os
+
+print(os.name)                                  # print: nt
+print(os.environ['JAVA_HOME'])                  # C:\Program Files\Java\jdk1.7.0_79
+print(os.getlogin())                            # print: hungnv132
+print(os.listdir('./test_folder'))              # ['01 text.txt', 'nested_folder']
+
+
