1bb575dfad3c3681deea0ba7c8ce15fd52a855f6
hungnv132 <hungnv132@gmail.com>
2017-03-19 15:00:23 +0700

    demo

--- basic_python/generators.py
+++ basic_python/generators.py
@@ -5,7 +5,7 @@ def generator(N):
     for x in range(N):
         yield x*2
 
-if __name__ == '__main__':
+def test():
     for x in generator(5):
         print(x)
 
@@ -14,3 +14,14 @@ if __name__ == '__main__':
 
     print(bin(88))
     print(bool({}))
+
+if __name__ == '__main__':
+
+    generator = (x * x for x in range(3))
+    print(type(generator))
+    print('length: %s' % len(generator))
+    for i in generator:
+        print('3')
+    print('length: %s' % len(generator))
+    for i in generator:
+        print(i)
