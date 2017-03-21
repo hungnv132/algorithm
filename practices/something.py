28d70e7d9aec7d54e96d765d1fa8f8542ccb2fad
hungnv132 <hungnv132@gmail.com>
2017-03-21 16:33:28 +0700

    add method 'hash_code()'

--- practices/something.py
+++ practices/something.py
@@ -18,7 +18,18 @@ def test_remove_all_stack():
     remove_all_stack(stack)
     print(len(stack))
 
+
+def hash_code(s):
+    mask = (1 << 32) - 1  # limit to 32-bit integers
+    h = 0
+    for character in s:
+        h = (h << 5 & mask) | (h >> 27)  # 5-bit cyclic shift of running sum
+    h += ord(character)  # add in value of next character
+    return h
+
 if __name__ == '__main__':
 
     print(ord('A'))
     print(list(map(ord, 'spam')))
+    print(hash_code("hungnv132"))
+
