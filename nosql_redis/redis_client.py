ebb872d9a1c7752147790b1f2bee9b15945398db
hungnv132 <hungnv132@gmail.com>
2017-03-23 13:01:02 +0700

    add  'from .'

--- nosql_redis/redis_client.py
+++ nosql_redis/redis_client.py
@@ -1,5 +1,5 @@
 import redis
-from config import ConnectionInfo
+from .config import ConnectionInfo
 # r = redis.StrictRedis(host='localhost', port=6379, db=0)
 # r.set('name', 'hungnv132')
 # print (r.get('age'))
