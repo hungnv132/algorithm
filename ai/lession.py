5cf04b320fad0145537e45de82fe8a7f3e256f1e
hungnv132 <hungnv132@gmail.com>
2017-06-23 00:04:47 +0700

    create file 'lession'

---
+++ ai/lession.py
@@ -0,0 +1,9 @@
+"""
+I. Supervised Learning (Học có giám sát)
+    - data -> label
+    - Classification: Label is limited,
+    - Regression: Label is a specific value
+II. Unsupervided Learning (Học không giám sát)
+    - No label
+
+"""
