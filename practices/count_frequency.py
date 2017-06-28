08a98fa260ae01006bcd0705dce9e5f302b507bf
hungnv132 <hungnv132@gmail.com>
2017-06-28 14:10:25 +0700

    add checking seeding

--- practices/count_frequency.py
+++ practices/count_frequency.py
@@ -1,3 +1,5 @@
+from functools import cmp_to_key
+
 data = [
     {'id': 2, 'count': 1},
     {'id': 12, 'count': 1},
@@ -13,21 +15,45 @@ data = [
     {'id': 34, 'count': 12},
 ]
 
+
+def check_seeding(id):
+    return True if id in [2, 10, 15, 88, 81, 1, 34] else False
+
+
+def compare(item1, item2):
+    return cmp_to_key(item1['count'], item2['count'])
+
+
 def count_frequency(data):
     print("Length of data: " + str(len(data)))
     for index, item in enumerate(data):
         if index == 0:
             last_count = item['count']
             times = 1
+            if check_seeding(item['id']):
+                count_seeding = 1
+            else:
+                count_seeding = 0
+
             continue
         if last_count == item['count']:
             times += 1
+
+            if check_seeding(item['id']):
+                count_seeding += 1
         else:
-            print("{} count - {} ".format(last_count, times))
+            print("{} count - {} - {} seeding ".format(last_count, times, count_seeding))
             last_count = item['count']
             times = 1
+
+            if check_seeding(item['id']):
+                count_seeding = 1
+            else:
+                count_seeding = 0
+
         if index == len(data) - 1:
-            print("{} count - {} ".format(last_count, times))
+            print("{} count - {} - {} seeding".format(last_count, times, count_seeding))
 
 if __name__ == '__main__':
     count_frequency(data)
+    print(sorted(data, key=compare))
