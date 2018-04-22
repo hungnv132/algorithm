2a26883a8f62ee196c7df9d3a81759b437449a99
hungnv132 <hungnv132@gmail.com>
2018-04-22 19:18:23 +0700

    59 specific ways to write better python

---
+++ basic_python/Effective python: 59 ways to write better python/01_pythonic_thinking.py
@@ -0,0 +1,13 @@
+"""
++ Item 1: Know which version of python you're using.
++ Item 2: Follow the PEP 8 style guide.
+  - Use space for indentation
+  - 4 spaces for each level
+  - 79 characters
+  - Continuation of long expressions onto additional lines indented by 4 extra
+    space from normal indentation level.
+  - In a file, functions and classes separated by 2 blank lines.
+  - In a class
+"""
+def item_1():
+    print("Item 1: Know which version of python you're using. ")
